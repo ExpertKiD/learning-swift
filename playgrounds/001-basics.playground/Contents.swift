7f73bfb5be10e98ebc60c3a6fd26ebc8cc495bdc
Suman Adhikari <expertxkid@gmail.com>
2022-01-24 23:19:36 +0545

    feat: add 001-basics playground and guide

---
+++ playgrounds/001-basics.playground/Contents.swift
@@ -0,0 +1,26 @@
+
+// 1. Defining and declaring variables
+var age:Int = 27
+var name: String = "Ariel"
+var initial: Character = "A"
+var bankBalance: Float = 21.60
+var isAdmin: Bool = true
+
+
+// 2. Rewrite with type inference
+var age1 = 27
+var name1 = "Ariel"
+var initial1 = "A"
+var bankBalance1 = 21.60
+var isAdmin1 = true
+
+// 3. Constants
+let hoursInADay = 24
+
+// 4. Comments
+// Single line comment
+
+/*
+Multi-line
+Comments
+*/
